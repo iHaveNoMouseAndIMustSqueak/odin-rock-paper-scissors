90e3048e4dc097222f00fb4d71346b4f87011cdc
Ben Newboult <ben.newboult@googlemail.com>
2020-03-16 01:31:19 +0000

    Plays a single round and returns winner with alert

--- app.js
+++ app.js
@@ -1,39 +1,84 @@
-/*class Player {
-    constructor(name) {
+class Player {
+    constructor(name = 'Player') {
         this._name = name;
-        this._play = '';
+        this._choice = '';
         this._score = 0;
     }
 
     get name() {return this._name}
-    get play() {return this._play}
+    get choice() {return this._choice}
     get score() {return this._score}
 
-    playHand(hand = Math.floor(Math.random()*3)) {
-        switch (hand) {
-            case 'Rock':
-            case 'rock':
-            case 0:
-                this._play = 'Rock';
-                break;
-            case 'Paper':
-            case 'paper':
-            case 1:
-                this._play = 'Paper';
-                break;
-            case 'Scissors':
-            case 'scissors':
-            case 2:
-                this._play = 'Scissors';
-                break;
-            default:
-                alert('Error! Please enter "rock", "paper", or "scissors"')
-                break;
-        }
+    set choice(choice) {this._choice = choice}
+    set score(score) {this._score = score}
+
+    playerChoice(e) {
+        /*playerChoiceListener(e => {
+            const displayPlayerSelection = document.querySelector('#display-results .player');
+            displayPlayerSelection.classList = 'player'; //Resets class list to just .player
+            displayPlayerSelection.classList.add(e.target.value);
+            this.choice = e.target.value;
+        })*/
+        const displayPlayerSelection = document.querySelector('#display-results .player');
+        displayPlayerSelection.classList = 'player'; //Resets class list to just .player
+        displayPlayerSelection.classList.add(e.target.value);
+        this.choice = e.target.value;
     }
 
-    incrementScore() {this._score++}
-    resetScore() {this._score = 0}
+    incrementScore() {
+        this.score++;
+    }
+
+    resetScore() {
+        this.score = 0;
+    }
+}
+
+class Computer extends Player {
+    constructor(name = 'Computer') {
+        super(name);
+    }
+
+    playerChoice() {
+        /*playerChoiceListener(e => {
+            const randomNumber = Math.floor(Math.random()*3);
+            switch (randomNumber) {
+                case 0:
+                    this.choice = 'rock';
+                    break;
+                case 1:
+                    this.choice = 'paper';
+                    break;
+                case 2:
+                    this.choice = 'scissors';
+                    break;
+                default:
+                    alert('Error!')
+                    break;
+            }
+            const displayComputerSelection = document.querySelector('#display-results .computer');
+            displayComputerSelection.classList = 'computer'; //Resets class list to just .computer
+            displayComputerSelection.classList.add(this.choice);
+        })*/
+        const randomNumber = Math.floor(Math.random()*3);
+        switch (randomNumber) {
+            case 0:
+                this.choice = 'rock';
+                break;
+            case 1:
+                this.choice = 'paper';
+                break;
+            case 2:
+                this.choice = 'scissors';
+                break;
+            default:
+                alert('Error!')
+                break;
+        }
+        const displayComputerSelection = document.querySelector('#display-results .computer');
+        displayComputerSelection.classList = 'computer'; //Resets class list to just .computer
+        displayComputerSelection.classList.add(this.choice);
+    }
 }
 
 class Game {
@@ -54,18 +99,13 @@ class Game {
 
     resetResults() {this._results = []}
 
-    playRound(handOne, handTwo) {
-        this.playerOne.playHand(handOne);
-        this.playerTwo.playHand(handTwo);
-
+    playRound() {
         let roundWinner;
         let roundLoser;
-        console.log(this.playerOne.play, this.playerTwo.play)
-
-        if((this.playerOne.play === 'Rock' && this.playerTwo.play === 'Scissors') || (this.playerOne.play === 'Paper' && this.playerTwo.play === 'Rock') || (this.playerOne.play === 'Scissors' && this.playerTwo.play === 'Paper')) {
+        if((this.playerOne.choice === 'rock' && this.playerTwo.choice === 'scissors') || (this.playerOne.choice === 'paper' && this.playerTwo.choice === 'rock') || (this.playerOne.choice === 'scissors' && this.playerTwo.choice === 'paper')) {
             roundWinner = this.playerOne;
             roundLoser = this.playerTwo;
-        } else if((this.playerTwo.play === 'Rock' && this.playerOne.play === 'Scissors') || (this.playerTwo.play === 'Paper' && this.playerOne.play === 'Rock') || (this.playerTwo.play === 'Scissors' && this.playerOne.play === 'Paper')) {
+        } else if((this.playerTwo.choice === 'rock' && this.playerOne.choice === 'scissors') || (this.playerTwo.choice === 'paper' && this.playerOne.choice === 'rock') || (this.playerTwo.choice === 'scissors' && this.playerOne.choice === 'paper')) {
             roundWinner = this.playerTwo;
             roundLoser = this.playerOne;
         } else {
@@ -74,36 +114,28 @@ class Game {
             return; // Exits early as 'roundWinner' and 'roundLoser' aren't defined and would cause error in next step
         }
         roundWinner.incrementScore();
-        this.results.push(`${roundWinner.play} beats ${roundLoser.play}. ${roundWinner.name} wins!`);
-        alert(`${roundWinner.play} beats ${roundLoser.play}. ${roundWinner.name} wins!`);
-    }
-
-    playGame(rounds = this.rounds) {
-        let roundsLeft = rounds;
-        this.playerOne.resetScore();
-        this.playerTwo.resetScore();
-        this.resetResults();
-        while(roundsLeft-- > 0) {
-            this.playRound(prompt('Rock, paper or scissors?', 'rock'));
-        }
-        alert(`Score:\n${this.playerOne.name} - ${this.playerOne.score}     ${this.playerTwo.name} - ${this.playerTwo.score}`)
+        this.results.push(`${roundWinner.choice} beats ${roundLoser.choice}. ${roundWinner.name} wins!`);
+        alert(`${roundWinner.choice} beats ${roundLoser.choice}. ${roundWinner.name} wins!`);
     }
 }
 
-const computer = new Player('Computer');
+const playerChoiceListener = callback => {
+    document.querySelector('#player-choice').addEventListener('click', e => {
+        if(e.target.type === 'button') {
+            callback(e);
+        }
+    })
+}
 
-const player = new Player('Player');
+const player = new Player('Player One');
+
+const computer = new Computer();
 
 const game1 = new Game([player, computer], 5);
 
-game1.playGame();*/
-
-
-const playerSelectButton = document.querySelector('#player-choice').addEventListener('click', e => {
-    if(e.target.type === 'button') {
-        const displayPlayerSelection = document.querySelector('#display-results .player');
-        displayPlayerSelection.classList = 'player'; //Resets class list to just .player
-        displayPlayerSelection.classList.add(e.target.value);
-        return e.target.value;
-    }
-});
+// Doesn't work if it's not invoked, obviously
+playerChoiceListener(e => {
+    player.playerChoice(e);
+    computer.playerChoice(e);
+    game1.playRound();
+})
